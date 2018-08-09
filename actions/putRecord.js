3663e83b7d7df59fcdefbeb45390c6671f6832aa
John Sokel <jsokel@journera.com>
2018-08-09 15:18:01 -0400

    Attempt at making kinesis respond to firehose PutRequest

--- actions/putRecord.js
+++ actions/putRecord.js
@@ -3,43 +3,22 @@ var BigNumber = require('bignumber.js'),
 
 module.exports = function putRecord(store, data, cb) {
 
-  var key = data.StreamName, metaDb = store.metaDb, streamDb = store.getStreamDb(data.StreamName)
+  var key = data.StreamName, metaDb = store.metaDb, streamDb = store.getStreamDb(data.DeliveryStreamName)
 
   metaDb.lock(key, function(release) {
     cb = release(cb)
 
-    store.getStream(data.StreamName, function(err, stream) {
+    store.getStream(data.DeliveryStreamName, function(err, stream) {
       if (err) return cb(err)
 
       if (!~['ACTIVE', 'UPDATING'].indexOf(stream.StreamStatus)) {
         return cb(db.clientError('ResourceNotFoundException',
-          'Stream ' + data.StreamName + ' under account ' + metaDb.awsAccountId + ' not found.'))
+          'Stream ' + data.DeliveryStreamName + ' under account ' + metaDb.awsAccountId + ' not found.'))
       }
 
       var hashKey, shardIx, shardId, shardCreateTime
 
-      if (data.ExplicitHashKey != null) {
-        hashKey = new BigNumber(data.ExplicitHashKey)
-
-        if (hashKey.comparedTo(0) < 0 || hashKey.comparedTo(new BigNumber(2).pow(128)) >= 0) {
-          return cb(db.clientError('InvalidArgumentException',
-            'Invalid ExplicitHashKey. ExplicitHashKey must be in the range: [0, 2^128-1]. ' +
-            'Specified value was ' + data.ExplicitHashKey))
-        }
-      } else {
-        hashKey = db.partitionKeyToHashKey(data.PartitionKey)
-      }
-
-      if (data.SequenceNumberForOrdering != null) {
-        try {
-          var seqObj = db.parseSequence(data.SequenceNumberForOrdering)
-          if (seqObj.seqTime > Date.now()) throw new Error('Sequence time in the future')
-        } catch (e) {
-          return cb(db.clientError('InvalidArgumentException',
-              'ExclusiveMinimumSequenceNumber ' + data.SequenceNumberForOrdering + ' used in PutRecord on stream ' +
-              data.StreamName + ' under account ' + metaDb.awsAccountId + ' is invalid.'))
-        }
-      }
+      hashKey = db.partitionKeyToHashKey('fake_partition_key')
 
       for (var i = 0; i < stream.Shards.length; i++) {
         if (stream.Shards[i].SequenceNumberRange.EndingSequenceNumber == null &&
@@ -75,13 +54,13 @@ module.exports = function putRecord(store, data, cb) {
 
         var record = {
           PartitionKey: data.PartitionKey,
-          Data: data.Data,
+          Data: data.Record.Data,
           ApproximateArrivalTimestamp: now / 1000,
         }
 
         streamDb.put(streamKey, record, function(err) {
           if (err) return cb(err)
-          cb(null, {ShardId: shardId, SequenceNumber: seqNum})
+          cb(null, {RecordId: seqNum})
         })
       })
     })
